411d90252a2df5e6510a97ae87900e8a8a7728dd
Emil Kjelsrud <emil.kjelsrud@intility.no>
2024-04-06 12:28:46 +0200

    Update cwc.rb to v.0.9.0

--- Formula/cwc.rb
+++ Formula/cwc.rb
@@ -1,8 +1,8 @@
 class Cwc < Formula
   desc "Chat with Code CLI tool"
   homepage "https://github.com/intility/cwc"
-  url "https://github.com/Intility/cwc/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "a43d766f4a00c3f339545a4dbd07890c182b5ddae238ca8cc303d34a169cd0f1"
+  url "https://github.com/Intility/cwc/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "9e52f1ee109e3d496245f4ce1c805a1b266eb091bb3ae8f0cb26e78212b46504"
   license "MIT"
 
   depends_on "go" => :build
